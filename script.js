0625fad4ec76237c06e0582eb2458c0a15981c96
Yevgeny <lesnoy022@gmail.com>
2019-07-14 10:32:10 +0300

    script
    add form validation

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+document.getElementById('sign_up').addEventListener("click", function (event) {
+    let data = document.getElementById('first-form');
+    if (data.checkValidity()) {
+        event.preventDefault();
+        formVisibility();
+    }
+});
+
 function formVisibility() {
-    document.getElementById("second-form").style.display = "block";
     document.getElementById("first-form").style.display = "none";
+    document.getElementById("second-form").style.display = "block";
 }
